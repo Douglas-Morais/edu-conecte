0fb0af5cdd84220d08c0fead2f2f9fe4372a0a3a
Douglas Morais <douglascarvalhomorais@hotmail.com>
2021-12-15 21:23:49 -0300

    remove app controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getConnectionOptions } from 'typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -15,7 +13,7 @@ import { AppService } from './app.service';
       useFactory: async () => Object.assign(await getConnectionOptions()),
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule { }
