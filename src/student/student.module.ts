0fbe4e141a0f8899af31f75d14f1e64e2507fb00
Douglas Morais <douglascarvalhomorais@hotmail.com>
2021-12-17 15:58:09 -0300

    create student repository and add into student module

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { StudentRepository } from './entity/student.repository';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([StudentRepository])
+  ],
+  controllers: [],
+  providers: [],
+  exports: []
+})
 export class StudentModule {}
