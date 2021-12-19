92165e47d69a9370c91051875994a7b8fb6962b1
Douglas Morais <douglascarvalhomorais@hotmail.com>
2021-12-19 16:08:31 -0300

    test search validation pipe

--- src/pipe/search-validation.pipe.spec.ts
+++ src/pipe/search-validation.pipe.spec.ts
@@ -1,7 +1,32 @@
+import { BadRequestException } from '@nestjs/common';
 import { SearchValidationPipe } from './search-validation.pipe';
 
 describe('SearchValidationPipe', () => {
+  let searchValidationPipe = new SearchValidationPipe();
+
   it('should be defined', () => {
-    expect(new SearchValidationPipe()).toBeDefined();
+    expect(searchValidationPipe).toBeDefined();
+  });
+
+  it('should be a string more than five chars', () => {
+    const fiveChars = '12345';
+    expect(searchValidationPipe.transform(fiveChars, null)).toEqual(fiveChars);
+    
+  });
+
+  it('Should be a string less than five chars', () => {
+    const twoChars = '12';
+    const threeChars = '123';
+    try {
+      searchValidationPipe.transform(twoChars, null);
+    } catch (error) {
+      expect(error).toBeInstanceOf(BadRequestException);
+    }
+
+    try {
+      searchValidationPipe.transform(threeChars, null);
+    } catch (error) {
+      expect(error).toBeInstanceOf(BadRequestException);
+    }
   });
 });
